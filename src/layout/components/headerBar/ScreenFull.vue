104686a9ac51948c5d99de1f163e71100bdff389
VANCEE LEE <1280403599@qq.com>
2022-09-10 00:15:26 +0800

    use screenfull

---
+++ src/layout/components/headerBar/ScreenFull.vue
@@ -0,0 +1,35 @@
+<template>
+  <div @click="onToggle">
+    <img
+      v-if="isFullscreen"
+      src="../../../assets/images/notFull.png"
+      style="width: 35px; height: 35px"
+    />
+    <img
+      v-else
+      src="../../../assets/images/isFull.png"
+      style="width: 35px; height: 35px"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import screenfull from 'screenfull'
+import { onMounted, onUnmounted, ref } from 'vue'
+const isFullscreen = ref(false)
+const change = () => {
+  isFullscreen.value = screenfull.isFullscreen
+}
+const onToggle = () => {
+  screenfull.toggle()
+}
+// 设置监听器
+onMounted(() => {
+  screenfull.on('change', change)
+})
+onUnmounted(() => {
+  screenfull.off('change', change)
+})
+</script>
+
+<style scoped></style>
--- src/layout/components/headerBar/index.vue
+++ src/layout/components/headerBar/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="head-bar">
     <el-row :gutter="24" style="display: flex; align-items: center">
-      <el-col :span="19" :offset="0">
+      <el-col :span="18" :offset="0">
         <!-- BreadCrumb -->
         <bread-crumb />
+      </el-col>
+      <!-- screenfull -->
+      <el-col :span="1">
+        <screen-full />
       </el-col>
       <!-- language -->
       <el-col :span="1">
@@ -21,6 +25,7 @@
 import BreadCrumb from './BreadCrumb.vue'
 import UserBar from './UserBar.vue'
 import LanguageBar from './LanguageBar.vue'
+import ScreenFull from './ScreenFull.vue'
 </script>
 
 <style scoped></style>
